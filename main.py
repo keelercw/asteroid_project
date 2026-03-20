0b1d2b81b23071982532f0f17ab07dcf06aec566
keelercw <dreamcatcher2154@gmail.com>
2026-03-19 19:54:26 -0500

    B: Added circles and Triangles. End of Homework Day 3/19/26

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pygame
-
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+
 def main():
-    print("Starting Asteroids with pygame version: 2.6.1")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
+        
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
+            
 if __name__ == "__main__":
     main()
