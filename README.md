406dc38b3e5365350d8537493011efa343920160
keelercw <dreamcatcher2154@gmail.com>
2026-03-20 14:17:49 -0500

    C: Added movement and Asteroids! End of Homework

---
+++ README.md
@@ -0,0 +1,1 @@
+Creating an Asteroids Clone with boot.dev
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     pygame.init()
@@ -9,7 +11,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    asteroid_field =AsteroidField()
+
+
 
     while True:
         log_state()
@@ -19,8 +33,12 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+
+        
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
