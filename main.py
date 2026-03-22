e12d05e5f492ce31fda605e16199438d0bb6488e
keelercw <dreamcatcher2154@gmail.com>
2026-03-22 14:09:46 -0500

    G: Added lives

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ def main():
     dt = 0
     font = pygame.font.Font(None, 36)
     WHITE = (255, 255, 255)
+    lives = 1
 
     score = 0
     def show_score(x, y):
         score_surface = font.render("Score: " +str(score), True, WHITE)
         screen.blit(score_surface, (x, y))
+
+    def show_lives(x, y):
+        lives_surface = font.render("Lives: " +str(lives), True, WHITE)
+        screen.blit(lives_surface, (x, y))
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -46,15 +51,21 @@ def main():
         
         screen.fill("black")
         show_score(10, 10)
+        show_lives(1150, 10)
         updatable.update(dt)
         for obj in drawable:
             obj.draw(screen)
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 log_event("player_hit")
-                print("Game over!")
-                print("Final Score: ", score)
-                sys.exit()
+                if lives > 0:
+                    player.kill()
+                    lives -= 1
+                    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+                else:
+                    print("Game over!")
+                    print("Final Score: ", score)
+                    sys.exit()
         for asteroid in asteroids:
             for shot in shots:
                 if shot.collides_with(asteroid):
